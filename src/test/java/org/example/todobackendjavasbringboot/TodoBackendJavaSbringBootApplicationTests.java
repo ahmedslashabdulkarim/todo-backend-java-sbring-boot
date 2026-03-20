311e3a8a79ccb1d0323f1981f0d92d5721842239
ahmed <ahmedslashabdulkarim@gmail.com>
2026-03-20 10:25:26 +0100

    Initial commit

---
+++ src/test/java/org/example/todobackendjavasbringboot/TodoBackendJavaSbringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.todobackendjavasbringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBackendJavaSbringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
